a78e7311ee7b4609f39054d96da85939c7822126
Felipedev2-0 <filbarros@hotmail.com>
2024-05-06 14:54:22 -0300

    rotas_view

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,45 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict";
 
         return Controller.extend("zov.controller.View1", {
-            onInit: function () {
-                //alert("onInit")
-
+            onInit: function(){
             },
 
-            onBeforeRendering: function () {
-                //alert("onBeforeRendering")
+            onNewCustomer: function(){
+                var r = sap.ui.core.UIComponent.getRouterFor(this)
+                r.navTo("RouteCustomerNew")
             },
 
-            onAfterRendering: function () {
-                //alert("onAfterRendering")
-            },
-
-            onExit: function () {
-                alert("onExit")
-            },
-
-            onCalcular: function () {
-                //padrão de declaracao das variaveis é primeiro o tipo, depois o nome//
-                var oView = this.getView()
-                var iB1 = parseInt(oView.byId("b1").getValue())
-                var iB2 = parseInt(oView.byId("b2").getValue())
-                var iB3 = parseInt(oView.byId("b3").getValue())
-                var iB4 = parseInt(oView.byId("b4").getValue())
-                var fResultado = 0
-
-                fResultado = (iB1 + iB2 + iB3 + iB4) / 4
-
-                oView.byId("resultado").setValue(fResultado)
-
-                MessageToast.show("Resultado = " + fResultado)
+            onEditCustomer1: function(){
+                var r = sap.ui.core.UIComponent.getRouterFor(this)
+                r.navTo("RouteCustomerEdit", { CustomerId: 1 })
             }
 
         });
